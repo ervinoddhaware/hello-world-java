6ebeb50b45ec13dba9d3b5f9ee1b5ff9fa5e672c
ervinoddhaware <er.vinoddhaware@gmail.com>
2026-04-30 14:45:43 +0530

    added comments and improved HelloWorld Program

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,8 @@
+//My first java program
+//Name: Vinod
+//Date: 30 april 2026
+//Goal: Learning java full stack development
+
 public class HelloWorld {   // 1 class declaration
     public static void main(String arg[])  // main method starting pt of the prog.
     {
@@ -8,5 +13,8 @@ public class HelloWorld {   // 1 class declaration
         System.out.println("I am learing java full stack");
         System.out.println("This is my first program");
         System.out.println("=================================");
+
+        //Extra line for practice
+        System.out.println("\nI will become a full stack developer in 9-12 months!");
     }
 }
