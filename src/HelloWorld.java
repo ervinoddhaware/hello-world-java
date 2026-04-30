f60d0331dfeb84765dbda695de25fa97948639c8
ervinoddhaware <er.vinoddhaware@gmail.com>
2026-04-30 15:18:47 +0530

    Improved HelloWorld program with better variable names and formatting

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,28 +1,29 @@
-//My first java program
-//Name: Vinod
-//Date: 30 april 2026
-//Goal: Learning java full stack development
+// My First Java Program with Variables
+// Name: Vinod Dhaware
+// Date: 30 April 2026
+// Goal: Learning Java Full Stack Development
 
-public class HelloWorld {   // 1 class declaration
-    public static void main(String arg[])  // main method starting pt of the prog.
-    {
-        //variable declare kar rahe hai
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Declaring Variables
         String name = "Vinod Dhaware";
         String city = "Pune";
         String goal = "Java Full Stack Developer";
-        int month = 10;
+        int months = 10;
         boolean isBeginner = true;
 
-        System.out.println("=================================");
+        System.out.println("=====================================");
         System.out.println("Hello World!");
         System.out.println("My name is " + name);
         System.out.println("I live in " + city + " city.");
-        System.out.println("I am learing to become a " + goal);
-        System.out.println("I will become job ready in "+ month +" months.");
-        System.out.println("Am i beginner? "+ isBeginner);
-        System.out.println("=================================");
+        System.out.println("I am learning to become a " + goal);
+        System.out.println("I will become job ready in " + months + " months.");
+        System.out.println("Am I a beginner? " + isBeginner);
+        System.out.println("=====================================");
 
-        //Extra line for practice
-        System.out.println("\nI will become a full stack developer in 9-12 months!");
+        // Extra message with new line
+        System.out.println("\nI will become a Full Stack Developer in 9-12 months!");
     }
 }
