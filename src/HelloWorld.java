ea3f7e59d8d1acda69a2549f9c1eb11e1a5a0232
ervinoddhaware <er.vinoddhaware@gmail.com>
2026-04-30 14:26:46 +0530

    added some info

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,12 @@
-public class HelloWorld {
-    public static void main(String arg[])
+public class HelloWorld {   // 1 class declaration
+    public static void main(String arg[])  // main method starting pt of the prog.
     {
-    System.out.println("Hello World");
+        System.out.println("=================================");
+        System.out.println("Hello World!");
+        System.out.println("My name is Vinod");
+        System.out.println("I live in Pune city");
+        System.out.println("I am learing java full stack");
+        System.out.println("This is my first program");
+        System.out.println("=================================");
     }
 }
